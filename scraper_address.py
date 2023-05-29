203d4eb14a5617a2457b56c4f1317d3b2be25332
Tryd0g0lik <work80@mail.ru>
2023-05-29 11:01:57 +0700

    file 'scraper_address.py' added and crreted 'self.name', 'self.type_name', 'self.reiting' and 'self.count' varieble  of the 'iterating_over_company' class

---
+++ scraper_address.py
@@ -0,0 +1,121 @@
+from scraper_gis import Gis_page
+import re
+class ScraperEachAddress(Gis_page):
+	"""
+	TODO: viewing each address
+	:properties: 'name' it's the name company;
+	:properties: 'snijgp' it's the comments/reviews for a company
+	:properties: 'geometry_name' it's  the companu's address
+	:properties: 'phone' it's the company number phone
+	:properties: 'email' it's the e-mail address
+	:properties: 'work_mode' it's the time by which the company is working
+	:properties: 'website' it's the URL of the site
+	:properties: 'lat' it's the data coordinates about the width
+	:properties: 'lon' it's the data coordinates about the long
+	:properties: 'vk', tg', 'wa' it's the social network
+	"""
+	def __init__(self, sity:str='', search_word:str = ''):
+		super().__init__(sity, search_word)
+		ScraperEachAddress.start_working(self)
+		self.name :str = ''
+		self.type_name:str = '' # тип - под названием
+		self.reiting:str = "" # Рейтинг
+		self.count:str = "" # кол-во 
+		
+		self.snijgp :str = '' #  краткое описание См. "описание.png"
+		self.geometry_name :str = ''
+		self.phone :str = ''
+		self.email :str = ''
+		self.work_mode :str = ''
+		self.website :str = ''
+		self.lat :str = '' # широта
+		self.lon :str = ''  # долгота
+		self.vk :str = '' # ВКонтакте
+		self.tg :str = ''  # Telegram
+		self.wa :str = '' # WhatsApp
+		ScraperEachAddress.iterating_over_company(self, self.object_soup)
+
+	def iterating_over_company(self, page):
+		'''
+		TODO: перебераем каждую найденую по запросу компанию
+		:rpoperties: 'titleGisReference' getting the url for  a company. It's the URL from the primary common
+		 column on the www-page.
+		:return:
+		'''
+
+		reg_text = r'(<div><div class=[\'|\"]{1}_{1}[\w]{3,10}[\'|\"]{1}>)'
+		strip_text = r'''(^<div class=[\'|\"]{1}_{1}[\w]{3,10}[\'|\"]{1})'''
+		strip_text_separator = re.match(strip_text, str(page)).group()
+		lstrip_text = rf'({strip_text_separator})'
+
+
+
+
+		if len(str(page)) > 0:
+			match_list = str(page).strip()
+			print("page: ", )
+			match_list = match_list.lstrip(lstrip_text).lstrip(">")
+			reg_text_separator = re.match(reg_text, str(match_list)).group()
+
+			match_list = match_list.replace(str(reg_text_separator), '_none_')
+			match_list = (match_list.split("_none_"))[1:]
+
+
+			# print("match: ", (match_list))
+
+
+			for one_company in match_list:
+				for one_separate in one_company.split("</div><div"):
+					one_separate = one_separate.lstrip()
+
+					reg_link_text = r'''(<a\sclass=[\"|\']_\w{3,10}[\"|\']\shref=[\"|\'][\/\w]*[\"|\']><span)'''
+					reg_titleGisReference = r'''([\"|\']\/\w*\/?[\w\/]*\/?[\"|\']?)'''
+
+					# reg_name = r'''(<span class=[\"|\']_\w{3,10}[\"|\'])'''
+					reg_name = r'''(<span class=[\"|\']_\w{3,10}[\"|\']>[\w|\W]{2,100}</span> ?[^(<!-)])'''
+
+					reg_type_name = r'''(^ class=[\"|\']_\w{3,10}[\"|\']><span class=[\"|\']_\w{3,10}[\"|\']>[^(<!-)][\w|\W]{2,100}<\/span> ?)''' #[^(!--)]
+					# print("one_separate: ", one_separate)
+					if bool(re.search(reg_link_text, str(one_separate))):
+						# print("one_separate: ", one_separate)
+						link_text = re.search(reg_link_text, str(one_separate)).group()
+						titleGisReference = "https://2gis.ru{}".format((re.search(reg_titleGisReference, str(link_text)).group()).strip('"').strip("'"))
+						print("titleGisReference: ", titleGisReference)
+
+					if bool(re.search(reg_name, str(one_separate))):
+						# print("one_separate: ", one_separate)
+						name = str(re.search(reg_name, str(one_separate)).group())
+						# print("name: ", name)
+						self.name ="{}".format((name.lstrip(r'''(<span class=[\"|\']_\w{5,10}[\"|\']>)''').lstrip('f"><span>')).replace('</span>', ""))
+						print("self.name: ",self.name)
+
+					elif bool(re.search(reg_type_name, str(one_separate))):
+						# print("one_separate: ", one_separate.lstrip())
+
+						type_name = str(re.search(reg_type_name, str(one_separate)).group())
+						type_name = re.search(r"([\w|\W]{3,100}<)", type_name).group().rstrip("<").strip()
+						type_name_separator = re.search(r"""(^class=[\"|\']_\w{3,10}[\"|\']><span class=[\"|\']_\w{3,10}[\"|\']>)""", type_name).group().__str__()
+						self.type_name = "{}".format(type_name.lstrip(str(type_name_separator)))
+						print("type_name:",self.type_name)
+					# print("reg_type_name: ", re.search(reg_type_name, str(one_separate)))
+
+					elif bool(re.search(r'(^class=\"_\w{3,10}\">[0-5]{1,2}.?[0-9]{0,2}[^( \W)])', one_separate)):
+						# print("one_separate: ", one_separate)
+						reiting = re.search(r'(^class=\"_\w{3,10}\">[0-5]{1,2}.?[0-9]{0,2}[^( \W)])', one_separate).group()
+						self.reiting = "{}".format(re.search(r'([0-5]{1,2}.?[0-9]{0,2}$)', reiting).group())
+
+						print("self.reiting: ", self.reiting)
+
+						
+					elif bool(re.search(r'(^class=\"_\w{3,10}\">[0-9]{1,2})', one_separate)):
+						self.count = "{}".format(re.search(r'(>([0-9]{0,2} [оценокблва]{0,10}))', one_separate).group().lstrip(">"))
+
+						print("self.count: ", self.count)
+
+					elif bool(re.search(r'((^[А-ЯЁ]{1}[а-яА-ЯёЁ]{3,50})[ |,]{1}([а-яА-ЯёЁ]{3,50}|[0-9]{1,2}[,| |\/]?[0-9]{0,2}))', one_separate[71:])):
+						print("address: ", re.search(r'((^[А-ЯЁ]{1}[а-яА-ЯёЁ]{3,50})[ |, ]{1}[^(\&nbsp;)]?([а-яА-ЯёЁ ,0-9]{3,70})[^(\&nbsp;)]?[ |, ]?)', one_separate[71:]).group())
+
+						# print("count: ", one_separate)
+				break
+
+
--- scraper_gis.py
+++ scraper_gis.py
@@ -6,57 +6,71 @@ from urllib.parse import unquote, quote
 
 
 class Gis_page():
+	"""
+	:properties: 'sity_name' - it's сity name
+
+	"""
 	def __init__(self, sity:str='', search_word:str=''):
-		self.sity = sity;
+		self.сity_name = sity;
 		self.search_word = search_word;
+		# Gis_page.start_working(self)
 
 	def __search_church(self): # search the word
-		sity = self.sity.__str__()
-		word = self.search_word.__str__()
+		sity = self.сity_name.__str__().strip()
+		word = self.search_word.__str__().strip()
 
-
-		headers = urls.HTTPHeaderDict()
-		headers.add('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.603 Yowser/2.5 Safa',)
-		headers.add('sec-ch-ua-platform', '"Windows"')
-		headers.add('Host', '2gis.ru')
-		headers.add('Origin', 'https://2gis.ru')
-		headers.add('sec-ch-ua', '"Chromium";v="110", "Not A(Brand";v="24", "YaBrowser";v="23"')
-
-		word_sity = str(self.sity).strip()
-		search_word = self.search_word.strip()
-
+		self.headers = urls.HTTPHeaderDict()
+		self.headers.add('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.603 Yowser/2.5 Safa',)
+		self.headers.add('sec-ch-ua-platform', '"Windows"')
+		self.headers.add('Host', '2gis.ru')
+		self.headers.add('Origin', 'https://2gis.ru')
+		self.headers.add('sec-ch-ua', '"Chromium";v="110", "Not A(Brand";v="24", "YaBrowser";v="23"')
 
 		requ_word = quote(word)
-		headers.add('Referer', f"https://2gis.ru/{sity}/search/{requ_word}")
+		self.headers.add('Referer', f"https://2gis.ru/{sity}/search/{requ_word}")
+		header = self.headers
+
 		t = urls.request("get", url=f"https://2gis.ru/{sity}/search/{requ_word}",
 		                 decode_content=True,
 		                 timeout=3,
-		                 headers=headers)
+		                 headers=header)
 
-		# if t.status == 200:
-		self.pages = unquote(t.data)
+		self.pages:str = ''
+		if t.status == 200:
+			self.pages = unquote(t.data)
 
 
+	def __scrap_gis(self,):
+		'''
+		TODO:  viewing the basis column
+		:return:
+		'''
 
-	def __scrap_gi(self,): # viewing
 		Gis_page.__search_church(self)
-		sity = (self.sity).lower()
+		sity = (self.сity_name).lower()
 		print(f"<a href='/{sity}")
-		response_text = self.pages
-		soup = beauty(response_text, features="html.parser")
-		self.object_soup = soup.find_all(id='root')[0] \
-			.find(name="div") \
-			.find(name="div") \
-			.find_all(name="div")[0] \
-			.find(name="div") \
-			.contents[1].contents[0].contents[0].contents[1].contents[0] \
-			.contents[0].contents[0].contents[1].contents[1].contents[0] \
-			.contents[0].find(name="a").find_parent("div").find_parent("div") \
-			.find_parent('div').find_parent("div")
+
+		if self.pages != '':
+			response_text = self.pages
+			soup = beauty(response_text, features="html.parser")
+			self.object_soup = soup.find_all(id='root')[0] \
+				.find(name="div") \
+				.find(name="div") \
+				.find_all(name="div")[0] \
+				.find(name="div") \
+				.contents[1].contents[0].contents[0].contents[1].contents[0] \
+				.contents[0].contents[0].contents[1].contents[1].contents[0] \
+				.contents[0].find(name="a").find_parent("div").find_parent("div") \
+				.find_parent('div').find_parent("div")
+
 
 	def save_files(self): # save the search's result into the file
-		Gis_page.__scrap_gi(self)
+		Gis_page.__scrap_gis(self)
 		with open( file=f"test_file.txt", encoding="utf-8",  mode="w") as f:
 			print(str(self.object_soup))
 			f.write(str(self.object_soup))
 			f.close()
+
+	def start_working(self):
+		return Gis_page.__scrap_gis(self)
+
